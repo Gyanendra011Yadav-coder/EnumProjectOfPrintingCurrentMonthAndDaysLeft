540180b38a9f62ca75b8897dee32499171bbc645
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2020-10-28 12:35:19 +0530

    CREATED THE CLASS TO TAKE THE USER INPUT AND TO WRITE A PROGRAM.

---
+++ src/main/MainClass.java
@@ -0,0 +1,13 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Gyanendra Yadav (Gyanendra011Yadav-coder)
+ *   Date: 10/28/2020
+ *   Time: 12:34 PM
+ *   File: MainClass.java
+ */
+
+package main;
+
+public class MainClass {
+
+}
+
--- src/main/Months.java
+++ src/main/Months.java
@@ -1,17 +1,24 @@
 package main;
 
 public enum Months {
-    January(),
-    Febuary(),
-    March(),
-    April(),
-    May(),
-    June(),
-    July(),
-    August(),
-    September(),
-    October(),
-    November(),
-    December()
+    January(31),
+    February(28),
+    March(31),
+    April(30),
+    May(31),
+    June(30),
+    July(31),
+    August(30),
+    September(31),
+    October(30),
+    November(31),
+    December(30);
+
+    int daysInMonths;
+    Months (int daysInMonths){
+        this.daysInMonths = daysInMonths;
+    }
+
+
     
 }
