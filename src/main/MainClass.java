b95c21076f153627fdfce803808501cc5f83cdf5
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2020-10-28 13:53:25 +0530

    CREATED THE SAME METHOD FOR OTHERS MONTHS FOR PRINTING THE CURRENT MONTH AND TOTAL NUMBER OF THE DAYS LEFT FOR COMPLETING THE MONTH.

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -45,8 +45,40 @@ public class MainClass {
 
                 System.out.println("THE DAYS LEFT IN COMPLETING THE MONTH IS" +
                         +(monthsObject.daysInMonths - date));
-
-
+            case JUNE:
+                System.out.println("THE CURRENT MONTH IS JUNE!");
+                System.out.println("THE DAYS LEFT IN COMPLETING THIS MONTH IS"
+                +(monthsObject.daysInMonths - date));
+            case JULY:
+                System.out.println("CURRENT MONTH IS JULY.");
+                System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
+                        +(monthsObject.daysInMonths - date));
+                break;
+            case AUGUST:
+                System.out.println("CURRENT MONTH IS AUGUST.");
+                System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
+                        +(monthsObject.daysInMonths - date));
+                break;
+            case SEPTEMBER:
+                System.out.println("CURRENT MONTH IS SEPTEMBER.");
+                System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
+                        +(monthsObject.daysInMonths - date));
+                break;
+            case OCTOBER:
+                System.out.println("CURRENT MONTH IS OCTOBER.");
+                System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
+                        +(monthsObject.daysInMonths - date));
+                break;
+            case NOVEMBER:
+                System.out.println("CURRENT MONTH IS NOVEMBER.");
+                System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
+                        +(monthsObject.daysInMonths - date));
+                break;
+            case DECEMEBR:
+                System.out.println("CURRENT MONTH IS DECEMBER.");
+                System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
+                        +(monthsObject.daysInMonths - date));
+                break;
             default:
 
 
