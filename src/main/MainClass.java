866b96757ca52d6adc3c0308678650b209fe1fbf
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2020-10-28 15:14:50 +0530

    CREATED THE METHOD TO PRINT THE NAME OF THE MONTH.

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -7,39 +7,46 @@
 
 package main;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 import java.util.Scanner;
-
 
 
 public class MainClass {
     public static void main(String[] args) {
         Scanner scannerObject = new Scanner(System.in);
         System.out.println("ENTER THE NAME OF THE CURRENT MONTH:-");
-        String monthsName = scannerObject.nextLine().trim().toUpperCase();
+        String monthsName= new GregorianCalendar().getDisplayName(
+                Calendar.MONTH,
+                Calendar.LONG,
+                Locale.US);
+//        scannerObject.nextLine().trim().toUpperCase();
+
         System.out.println("ENTER THE CURRENT DATE:-");
         int date = scannerObject.nextInt();
         Months monthsObject = Months.valueOf(monthsName);
-        switch (monthsObject){
+        switch (monthsObject) {
             case JANUARY:
                 System.out.println("CURRENT MONTH IS JANUARY.");
-                System.out.println("THE TOTAL NUMBER OF DAYS IN"+monthsName+",is "
-                        +monthsObject.daysInMonths);
+                System.out.println("THE TOTAL NUMBER OF DAYS IN" + monthsName + ",is "
+                        + monthsObject.daysInMonths);
                 System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
             case FEBRUARY:
                 System.out.println("CURRENT MONTH IS FEBRUARY.");
                 System.out.println("THE TOTAL NUMBER OF DAYS, LEFT IS"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
             case MARCH:
                 System.out.println("CURRENT MONTH IS MARCH.");
                 System.out.println("THE TOTAL NUMBER OF DAYS, LEFT IS"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
             case APRIL:
                 System.out.println("CURRENT MONTH IS APRIL!");
                 System.out.println("THE TOTAL NUMBER OF DAYS LEFT,IN COMPLETING THIS MONTH IS"
-                +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
             case MAY:
                 System.out.println("THE CURRENT MONTH IS MAY!");
 
@@ -48,44 +55,39 @@ public class MainClass {
             case JUNE:
                 System.out.println("THE CURRENT MONTH IS JUNE!");
                 System.out.println("THE DAYS LEFT IN COMPLETING THIS MONTH IS"
-                +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
             case JULY:
                 System.out.println("CURRENT MONTH IS JULY.");
                 System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
             case AUGUST:
                 System.out.println("CURRENT MONTH IS AUGUST.");
                 System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
             case SEPTEMBER:
                 System.out.println("CURRENT MONTH IS SEPTEMBER.");
                 System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
             case OCTOBER:
                 System.out.println("CURRENT MONTH IS OCTOBER.");
                 System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
             case NOVEMBER:
                 System.out.println("CURRENT MONTH IS NOVEMBER.");
                 System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
-            case DECEMEBR:
+            case DECEMEBER:
                 System.out.println("CURRENT MONTH IS DECEMBER.");
                 System.out.println("THE TOTAL NUMBER OF DAYS,LEFT IN COMPLETING THIS MONTH IS;"
-                        +(monthsObject.daysInMonths - date));
+                        + (monthsObject.daysInMonths - date));
                 break;
             default:
-
-
-
         }
-
-
         scannerObject.close();
     }
 
--- src/main/Months.java
+++ src/main/Months.java
@@ -2,27 +2,27 @@ package main;
 
 public enum Months {
     JANUARY(31),
-    FEBRUARY(28),
+    FEBRUARY(29),
     MARCH(31),
     APRIL(30),
     MAY(31),
     JUNE(30),
     JULY(31),
-    AUGUST(30),
-    SEPTEMBER(31),
-    OCTOBER(30),
-    NOVEMBER(31),
-    DECEMEBR(30);
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMEBER(31);
 
     int daysInMonths;
-    Months (int daysInMonths){
+
+    Months(int daysInMonths) {
         this.daysInMonths = daysInMonths;
     }
 
-    public int getDaysInMonths(){
+    public int getDaysInMonths() {
         return daysInMonths;
     }
 
 
-    
 }
