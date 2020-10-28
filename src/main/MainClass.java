fb0df46b34825b188db8ce522e112df255255307
gyanu <gyanendra.yadav_cs19@gla.ac.in>
2020-10-28 13:08:08 +0530

    CREATED THE SWITCH CASE METHOD TO PRINT THE STATEMENT.

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -9,11 +9,26 @@ package main;
 
 import java.util.Scanner;
 
+
+
 public class MainClass {
     public static void main(String[] args) {
         Scanner scannerObject = new Scanner(System.in);
+        System.out.println("ENTER THE NAME OF THE MONTH.");
         String monthsName = scannerObject.nextLine();
         Months monthsObject = Months.valueOf(monthsName);
+        switch (monthsObject){
+            case January:
+                System.out.println("CURRENT MONTH IS JANUARY.");
+                break;
+            case February:
+                System.out.println("CURRENT MONTH IS FEBRUARY.");
+                break;
+            case March:
+                System.out.println("CURRENT MONTH IS MARCH.");
+            default:
+
+        }
 
 
 
